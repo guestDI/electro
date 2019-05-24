72b0cec372962191cebac5bc4cef9d42ef900827
jack.bauer <ignatovich.dm@gmail.com>
2019-05-24 16:57:34 +0300

    Remove browser router

--- src/App.js
+++ src/App.js
@@ -2,18 +2,16 @@ import React from 'react';
 import Layout from './components/Layout/Layout';
 import WelcomeContainer from './containers/Welcome/WelcomeContainer'
 import ConditionsContainer from './containers/Conditions/ConditionsContainer'
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import CatalogContainer from "./containers/Catalog/CatalogContainer";
 
 function App() {
   return (
     <div >
         <Layout>
-            <BrowserRouter>
-                <Route path="/" exact component={WelcomeContainer}/>
-                <Route path="/conditions" component={ConditionsContainer}/>
-                <Route path="/equipment" component={CatalogContainer}/>
-            </BrowserRouter>
+            <Route path="/" exact component={WelcomeContainer}/>
+            <Route path="/conditions" component={ConditionsContainer}/>
+            <Route path="/equipment" component={CatalogContainer}/>
         </Layout>
     </div>
   );
